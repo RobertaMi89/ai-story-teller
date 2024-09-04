3972e0eba478842fe7c460413d6ba1df6a674de3
RobertaMi89 <roberta.miosi@gmail.com>
2024-09-04 17:36:26 +0200

    fix styles

--- src/components/Molecules/SelectBox/SelectBox.tsx
+++ src/components/Molecules/SelectBox/SelectBox.tsx
@@ -20,7 +20,7 @@ const SelectBox = (props: SelectBoxProps) => {
       <h3>{label}</h3>
       <select id="select" onChange={handleChange}>
         <option value="" hidden>
-          seleziona
+          Seleziona
         </option>
         {list.map((item) => (
           <option key={item.value} value={item.value}>
